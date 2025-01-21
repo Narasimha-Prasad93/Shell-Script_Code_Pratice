0fbc8abdf05820cab11778a5125a0e49f36845c9
unknown <narasimhaprasad023@gmail.com>
2025-01-21 15:12:05 +0530

    arguments scripts commited

---
+++ 05-arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Good Morning $PERSON2, How are you?"
+echo "$PERSON2: Mng $PERSON1, How are you?"
+
+## note - we need to pass person1, person2 values to be passed during program call
+## example - sh 05-arugments.sh ram raheem
