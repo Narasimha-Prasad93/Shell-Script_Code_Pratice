0b6072e3dc8e2de29fa7b4df982d990eb60e2dbc
unknown <narasimhaprasad023@gmail.com>
2025-01-21 14:28:28 +0530

    improved shell script using varaible concept assignment

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Good Morning $PERSON2, How are you?"
+echo "$PERSON2: Mng $PERSON1, How are you?"
+echo "$PERSON1: I am good $PERSON2, how are you $PERSON2?"
+echo "$PERSON2: Good $PERSON1, how is the your daugher?"
+echo "This is a general commit"
