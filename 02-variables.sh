7cdfbe811cb78a70c323d609e752cd6e35796a8c
unknown <narasimhaprasad023@gmail.com>
2025-01-21 13:43:37 +0530

    this is a general commit

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Ramesh: Good Morning Suresh, How are you?"
+echo "Suresh: Mng Ramesh, How are you?"
+echo "Ramesh: I am good Suresh, how are you suresh?"
+echo "Suresh: Good Ramesh, how is the your daugher?"
+echo "This is a general commit"
+
