48e119f9324de955fb552737a43e45897ff667d2
unknown <narasimhaprasad023@gmail.com>
2025-01-21 19:27:10 +0530

    array shell script committed

---
+++ 08-Arrays.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FRUITS=("apple" "banana" "mango")
+
+echo "I am :$FRUITS[0]"  ## this will print I am apple
+echo "I am :$FRUITS[1]"  ## this will print I am banana
+echo "I am :$FRUITS[2]"  ## this will print I am mango
+
+echo "We have collected: $FRUITS[@]"  ### This will print output as below - we have collected apple banana mango
+
+
+
+
