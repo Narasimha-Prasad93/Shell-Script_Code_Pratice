a40afa8173d6cf52f5da0c38844ed0b1f06ba5d2
unknown <narasimhaprasad023@gmail.com>
2025-01-22 18:07:13 +0530

    extending conditions shell script commits by adding complexcities to the script

---
+++ 10-conditions_1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo "You are not an root user"
+    exit 1 ## exit 1 command exits the execution of the above statement if condtion is fails.
+else
+    echo "You are the root user, please proceed installing the packages"
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]  ## $? is the exit status of the above command and it checks the success or failure and stores it in its storage
+then 
+    echo "Error :: installing mysql is FAILED"
+else 
+    echo "installing mysql is SUCCESS"
+fi
